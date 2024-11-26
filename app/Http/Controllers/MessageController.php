5c72b520b8c2cb208b1248663c12111b7f3d0597
RDEV-K <eye.shy3294@gmail.com>
2024-11-26 01:35:20 -0800

    #update daily

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -65,6 +65,19 @@ class MessageController extends Controller
         exit();
     }
 
+    public function quote(Request $request) {
+        $this->validate($request,[
+            'name'=>'string|required|min:2',
+            'email'=>'email|required',
+            'message'=>'required|min:20|max:200',
+            'subject'=>'string|required',
+            'phone'=>'numeric|required'
+        ]);
+        dd($request->name);
+
+        $message=Message::create($request->all());
+    }
+
     /**
      * Display the specified resource.
      *
